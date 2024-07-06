697a54044b20854e5177c93fea9371c644615508
markitokun <119384614+markitokun@users.noreply.github.com>
2024-07-06 13:59:18 -0600

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+// import './topics/1-basics-types.ts'
+// import './topics/2-object-interface.ts'
+// import './topics/3-functions.ts'
+// import './topics/4-homework-type'
+import './topics/5-basic-destructuring'
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+Hola mundo
+`
---
+++ src/topics/2-object-interface.ts
@@ -0,0 +1,24 @@
+
+let skills: string[] = ['Bash','Counter','Healing'];
+
+
+interface Character {
+    name: string;
+    hp: number;
+    skills: string[];
+    hometown?: string;
+}
+
+
+const strider: Character = {
+    name: 'strider',
+    hp: 100,
+    skills: ['Bash', 'Counter'],
+};
+
+strider.hometown = 'Rivendell';
+
+console.table(strider)
+
+
+export{};
